585a2fb3546c81134d026bdbdfc9035e54d43261
miridih-jyjang <jyjang@miridih.com>
2024-12-27 14:15:30 +0900

    initial commit

---
+++ demo/inference_demo_with_gt.py
@@ -0,0 +1,190 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+"""Image Demo.
+
+This script adopts a new inference class, currently supports image path,
+np.array and folder input formats, and will support video and webcam
+in the future.
+
+Example:
+    Save visualizations and predictions results::
+
+        CUDA_VISIBLE_DEVICES=0 python demo/inference_demo.py /data/miricanvas-ai-labs-official/02.Dataset/01.Find-meaning-groups/01.miricanvas/PPT-template_ko/three_groups_val.json projects/CO-DETR/configs/codino/co_dino_5scale_swin_l_16x
+        b6_16e_o365toCOCOtomiridih.py --weights /data/checkpoints/jjy/three_groups_swin_l_b3_16e/epoch_1.pth --image_path /workspace/data/decorate_detection/Raw/originSize_png
+
+    Visualize prediction results::
+
+        python demo/image_demo.py demo/demo.jpg rtmdet-ins-s --show
+
+        python demo/image_demo.py demo/demo.jpg rtmdet-ins_s_8xb32-300e_coco \
+        --show
+"""
+
+import ast
+import json
+import os
+from argparse import ArgumentParser
+
+from mmengine.logging import print_log
+
+from mmdet.apis import DetInferencer
+from mmdet.evaluation import get_classes
+from setproctitle import setproctitle
+import wandb, cv2
+from pycocotools.coco import COCO
+
+def visualize_ground_truth(image_path, annotations, coco):
+    image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+    for ann in annotations:
+        bbox = ann['bbox']
+        category_id = ann['category_id']
+        category_name = coco.loadCats(category_id)[0]['name']
+        
+        # Draw bounding box
+        x, y, w, h = map(int, bbox)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        
+        # Put label
+        cv2.putText(image, category_name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    
+    return image
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        'inputs', type=str, help='Input image file, folder path, or COCO JSON file.')
+    parser.add_argument(
+        'model',
+        type=str,
+        help='Config or checkpoint .pth file or the model name '
+        'and alias defined in metafile. The model configuration '
+        'file will try to read from .pth if the parameter is '
+        'a .pth weights file.')
+    parser.add_argument('--weights', default=None, help='Checkpoint file')
+    parser.add_argument(
+        '--out-dir',
+        type=str,
+        default='outputs',
+        help='Output directory of images or prediction results.')
+    parser.add_argument(
+        '--texts', help='text prompt, such as "bench . car .", "$: coco"')
+    parser.add_argument(
+        '--device', default='cuda:0', help='Device used for inference')
+    parser.add_argument(
+        '--pred-score-thr',
+        type=float,
+        default=0.3,
+        help='bbox score threshold')
+    parser.add_argument(
+        '--image_path', type=str, default="./", help='image path')
+    parser.add_argument(
+        '--batch-size', type=int, default=1, help='Inference batch size.')
+    parser.add_argument(
+        '--show',
+        action='store_true',
+        help='Display the image in a popup window.')
+    parser.add_argument(
+        '--no-save-vis',
+        action='store_true',
+        help='Do not save detection vis results')
+    parser.add_argument(
+        '--no-save-pred',
+        action='store_true',
+        help='Do not save detection json results')
+    parser.add_argument(
+        '--print-result',
+        action='store_true',
+        help='Whether to print the results.')
+    parser.add_argument(
+        '--palette',
+        default='none',
+        choices=['coco', 'voc', 'citys', 'random', 'none'],
+        help='Color palette used for visualization')
+    parser.add_argument(
+        '--custom-entities',
+        '-c',
+        action='store_true',
+        help='Whether to customize entity names? '
+        'If so, the input text should be '
+        '"cls_name1 . cls_name2 . cls_name3 ." format')
+
+    call_args = vars(parser.parse_args())
+    
+    # Handle COCO JSON input
+    if call_args['inputs'].endswith('.json'):
+        with open(call_args['inputs'], 'r') as f:
+            coco_data = json.load(f)
+        image_paths = [os.path.join(call_args['image_path'], coco_data['images'][i]['file_name']) for i in range(len(coco_data['images']))]
+        call_args['json_file'] = call_args['inputs']
+        call_args['inputs'] = image_paths
+        
+
+    del call_args['image_path']
+
+    if call_args['no_save_vis'] and call_args['no_save_pred']:
+        call_args['out_dir'] = ''
+
+    if call_args['model'].endswith('.pth'):
+        print_log('The model is a weight file, automatically '
+                  'assign the model to --weights')
+        call_args['weights'] = call_args['model']
+        call_args['model'] = None
+
+    init_kws = ['model', 'weights', 'device', 'palette']
+    init_args = {}
+    for init_kw in init_kws:
+        init_args[init_kw] = call_args.pop(init_kw)
+    
+    return init_args, call_args
+
+
+def main():
+    setproctitle('jooyoung-jang')
+    init_args, call_args = parse_args()
+    inferencer = DetInferencer(**init_args)
+
+    # Load COCO annotations
+    coco = COCO(call_args['json_file'])
+    del call_args['json_file']
+
+    # Create a new wandb table
+    table = wandb.Table(columns=["Index", "Visualization", "Ground Truth"])
+
+    if int(os.environ.get('LOCAL_RANK', 0)) == 0:
+        wandb.init(project='decorate_detection', entity='miridih-ailabs', name=f'evaluate_{init_args["model"]}_{init_args["weights"]}')
+    
+    result_dict, gt_dict = [], []
+    # Handle multiple image inputs from COCO JSON
+    if isinstance(call_args['inputs'], list):
+        for idx, image_path in enumerate(call_args['inputs']):
+            # if idx > 150:
+            #     break
+            call_args['inputs'] = image_path
+            output_dict = inferencer(**call_args)
+
+            # Get image id from file name
+            image_id = next((img['id'] for img in coco.dataset['images'] if img['file_name'] in image_path), None)
+            annotations = coco.loadAnns(coco.getAnnIds(imgIds=image_id))
+
+            # Visualize ground truth
+            gt_image = visualize_ground_truth(image_path, annotations, coco)
+            
+            template_idx = os.path.basename(image_path).split('.')[0]
+
+            if output_dict['visualization'] is not None:
+                table.add_data(template_idx, wandb.Image(output_dict['visualization'][0]), wandb.Image(gt_image))
+
+    else:
+        result_dict = inferencer(**call_args)
+    
+    wandb.log({"results_table": table})
+
+    if call_args['out_dir'] != '' and not (call_args['no_save_vis']
+                                           and call_args['no_save_pred']):
+        print_log(f'results have been saved at {call_args["out_dir"]}')
+    
+    if int(os.environ.get('LOCAL_RANK', 0)) == 0:
+        wandb.finish()
+
+
+if __name__ == '__main__':
+    main()
---
+++ projects/CO-DETR/configs/codino/co_dino_5scale_swin_l_16xb4_16e_o365toCOCOtoThreeGroups.py
@@ -0,0 +1,224 @@
+_base_ = ['co_dino_5scale_r50_8xb2_1x_coco.py']
+
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth'  # noqa
+load_from = 'https://download.openmmlab.com/mmdetection/v3.0/codetr/co_dino_5scale_swin_large_16e_o365tococo-614254c9.pth'  # noqa
+num_dec_layer = 6
+loss_lambda = 2.0
+num_classes = 3
+batch_size = 1
+dataset_type = 'ThreeGroupDataset'
+# model settings
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='SwinTransformer',
+        pretrain_img_size=384,
+        embed_dims=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        window_size=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.3,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        # Please only add indices that would be used
+        # in FPN, otherwise some parameter will not be used
+        with_cp=True,
+        convert_weights=True,
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+    neck=dict(in_channels=[192, 384, 768, 1536]),
+    query_head=dict(
+        type='CoDINOHead',
+        dn_cfg=dict(box_noise_scale=0.4, group_cfg=dict(num_dn_queries=500)),
+        num_classes=num_classes,
+        transformer=dict(encoder=dict(with_cp=6))),
+    roi_head=[
+        dict(
+            type='CoStandardRoIHead',
+            bbox_roi_extractor=dict(
+                type='SingleRoIExtractor',
+                roi_layer=dict(
+                    type='RoIAlign', output_size=7, sampling_ratio=0),
+                out_channels=256,
+                featmap_strides=[4, 8, 16, 32, 64],
+                finest_scale=56),
+            bbox_head=dict(
+                type='Shared2FCBBoxHead',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=num_classes,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=False,
+                reg_decoded_bbox=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0 * num_dec_layer * loss_lambda),
+                loss_bbox=dict(
+                    type='GIoULoss',
+                    loss_weight=10.0 * num_dec_layer * loss_lambda)))
+    ],
+    bbox_head=[
+        dict(
+            type='CoATSSHead',
+            num_classes=num_classes,
+            in_channels=256,
+            stacked_convs=1,
+            feat_channels=256,
+            anchor_generator=dict(
+                type='AnchorGenerator',
+                ratios=[1.0],
+                octave_base_scale=8,
+                scales_per_octave=1,
+                strides=[4, 8, 16, 32, 64, 128]),
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[.0, .0, .0, .0],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            loss_cls=dict(
+                type='FocalLoss',
+                use_sigmoid=True,
+                gamma=2.0,
+                alpha=0.25,
+                loss_weight=1.0 * num_dec_layer * loss_lambda),
+            loss_bbox=dict(
+                type='GIoULoss',
+                loss_weight=2.0 * num_dec_layer * loss_lambda),
+            loss_centerness=dict(
+                type='CrossEntropyLoss',
+                use_sigmoid=True,
+                loss_weight=1.0 * num_dec_layer * loss_lambda)),
+    ]
+        )
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(
+        type='RandomChoice',
+        transforms=[
+            [
+                dict(
+                    type='RandomChoiceResize',
+                    scales=[(480, 2048), (512, 2048), (544, 2048), (576, 2048),
+                            (608, 2048), (640, 2048), (672, 2048), (704, 2048),
+                            (736, 2048), (768, 2048), (800, 2048), (832, 2048),
+                            (864, 2048), (896, 2048), (928, 2048), (960, 2048),
+                            (992, 2048), (1024, 2048), (1056, 2048),
+                            (1088, 2048), (1120, 2048), (1152, 2048),
+                            (1184, 2048), (1216, 2048), (1248, 2048),
+                            (1280, 2048), (1312, 2048), (1344, 2048),
+                            (1376, 2048), (1408, 2048), (1440, 2048),
+                            (1472, 2048), (1504, 2048), (1536, 2048)],
+                    keep_ratio=True)
+            ],
+            [
+                dict(
+                    type='RandomChoiceResize',
+                    # The radio of all image in train dataset < 7
+                    # follow the original implement
+                    scales=[(400, 4200), (500, 4200), (600, 4200)],
+                    keep_ratio=True),
+                dict(
+                    type='RandomCrop',
+                    crop_type='absolute_range',
+                    crop_size=(384, 600),
+                    allow_negative_crop=True),
+                dict(
+                    type='RandomChoiceResize',
+                    scales=[(480, 2048), (512, 2048), (544, 2048), (576, 2048),
+                            (608, 2048), (640, 2048), (672, 2048), (704, 2048),
+                            (736, 2048), (768, 2048), (800, 2048), (832, 2048),
+                            (864, 2048), (896, 2048), (928, 2048), (960, 2048),
+                            (992, 2048), (1024, 2048), (1056, 2048),
+                            (1088, 2048), (1120, 2048), (1152, 2048),
+                            (1184, 2048), (1216, 2048), (1248, 2048),
+                            (1280, 2048), (1312, 2048), (1344, 2048),
+                            (1376, 2048), (1408, 2048), (1440, 2048),
+                            (1472, 2048), (1504, 2048), (1536, 2048)],
+                    keep_ratio=True)
+            ]
+        ]),
+    dict(type='PackDetInputs')
+]
+
+train_dataloader = dict(
+    batch_size=batch_size, num_workers=2, 
+    dataset=dict(
+        type=dataset_type,
+        pipeline=train_pipeline,
+        ann_file='/data/miricanvas-ai-labs-official/02.Dataset/01.Find-meaning-groups/01.miricanvas/PPT-template_ko/three_groups_32k_train.json',
+        data_prefix=dict(img='/data/miricanvas-ai-labs-official/02.Dataset/01.Find-meaning-groups/01.miricanvas/PPT-template_ko/decorate_detection/Raw/originSize_png/')))
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(2048, 1280), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+val_dataloader = dict(
+    batch_size=1, num_workers=2, 
+    dataset=dict(
+        type=dataset_type,
+        pipeline=test_pipeline,
+        ann_file='/data/miricanvas-ai-labs-official/02.Dataset/01.Find-meaning-groups/01.miricanvas/PPT-template_ko/three_groups_32k_val.json',
+        data_prefix=dict(img='/data/miricanvas-ai-labs-official/02.Dataset/01.Find-meaning-groups/01.miricanvas/PPT-template_ko/decorate_detection/Raw/originSize_png/')))
+
+test_dataloader = val_dataloader
+val_evaluator = dict(ann_file='/data/miricanvas-ai-labs-official/02.Dataset/01.Find-meaning-groups/01.miricanvas/PPT-template_ko/three_groups_32k_val.json')
+test_evaluator = val_evaluator
+optim_wrapper = dict(optimizer=dict(lr=1e-4))
+
+max_epochs = 16
+train_cfg = dict(max_epochs=max_epochs)
+
+param_scheduler = [
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[8],
+        gamma=0.1)
+]
+log_config = dict(
+    interval=10,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='MMDetWandbHook',
+             init_kwargs=dict(
+                 project='decorate_detection',
+                 entity='miridih-ailabs',
+                 name='three_groups_32k'),
+            interval=1,
+            log_checkpoint=True,
+            log_checkpoint_metadata=True,
+            num_eval_images=10,
+)
+    ])
+    
+vis_backends = [dict(type='WandbVisBackend',
+                    init_kwargs={
+                        'project': 'decorate_detection',
+                        'entity': 'miridih-ailabs',
+                        'name': 'three_groups_32k'  
+                    })]
+visualizer = dict(
+    type='DetLocalVisualizer',
+    vis_backends=vis_backends)
+    
+work_dir = '/data/checkpoints/jjy/three_groups_swin_l_b4_16e_32k'
+
